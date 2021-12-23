8407f8036d61329ee73ec82ca73ce34111f20cf9
Vinicius Carrijo <viniciuscarrijo@hotmail.com>
2021-12-23 16:04:53 -0300

    criando arquivos da camada domain

---
+++ src/main/java/edu/unifacef/employee/domains/Employee.java
@@ -0,0 +1,22 @@
+package edu.unifacef.employee.domains;
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+public class Employee {
+
+    private String id;
+    private String name;
+    private String birthday;
+    private String cpf;
+    private double salary;
+    private String function;
+    private String status;
+    private LocalDateTime createdDate;
+    private LocalDateTime lastModifiedDate;
+
+}
