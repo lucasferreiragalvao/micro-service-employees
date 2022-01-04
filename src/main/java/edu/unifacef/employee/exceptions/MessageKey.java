2c91fc67ed8bc8eb5db57503522f801a8cb20399
Vinicius Carrijo <viniciuscarrijo@hotmail.com>
2022-01-03 22:39:32 -0300

    adicionando endpoint patch

--- src/main/java/edu/unifacef/employee/exceptions/MessageKey.java
+++ src/main/java/edu/unifacef/employee/exceptions/MessageKey.java
@@ -9,7 +9,8 @@ public enum MessageKey {
 
     EMPLOYEE_NOT_FOUND("employee.not.found"),
     EMPLOYEE_ALREADY_EXISTS("employee.already.exists"),
-    DIFFERENT_EMPLOYEE_CPF("employee.different.cpf");
+    DIFFERENT_EMPLOYEE_CPF("employee.different.cpf"),
+    STATUS_NOT_FOUND("employee.status.not.found");
 
     private String key;
 }
